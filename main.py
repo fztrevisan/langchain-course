ced854ebef9d27a96421d83fe55117449d136dc4
fztrevisan <trevisan.fernando@gmail.com>
2026-06-01 11:40:43 -0600

    first chain

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+from input_data import INFORMATION
 
 load_dotenv()
 
 
 def main():
-    openai_api_key = os.getenv("OPENAI_API_KEY")
-    print(f"Olha a chave {openai_api_key}")
+    summary_template_str = """## Action
+Based on the info provided, create:
+1. A short summary;
+2. Two interesting facts about the person.
+
+## Information
+{information}
+"""
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template_str
+    )
+
+    llm = ChatOpenAI(
+        temperature=0,
+        model="gpt-4o",
+        base_url="https://models.inference.ai.azure.com",
+        api_key=os.getenv("GITHUB_GPT_PAT"),
+    )
+    chain = summary_prompt_template | llm
+    response = chain.invoke(input={"information": INFORMATION})
+
+    token_usage = response.response_metadata.get("token_usage", {})
+    print("Total Tokens:", token_usage.get("total_tokens"))
+    print("Prompt Tokens:", token_usage.get("prompt_tokens"))
+    print("Completion Tokens:", token_usage.get("completion_tokens"))
+
+    response.pretty_print()
 
 
 if __name__ == "__main__":
